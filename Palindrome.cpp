3cb29ca8d841bdec032b6633184cbb81e5e27db7
Janak Mallik <janaktoy@gmail.com>
2024-06-12 15:34:47 +0000

    palindrome

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,12 +1,38 @@
-public static boolean isPalindrome(String input) {
-    
-    if (input.length( ) == 0 || input.length( ) == 1) {
-        return true;
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class PalindromeChecker {
+public:
+    // Method to check if a string is a palindrome
+    bool isPalindrome(const string &input) const {
+        // Base cases: if the string is empty or has only one character, it's a palindrome
+        if (input.length() == 0 || input.length() == 1) {
+            return true;
+        }
+
+        // Recursive case: check the first and last characters
+        if (input[0] == input[input.length() - 1]) {
+            // Remove the first and last characters and check the remaining substring
+            return isPalindrome(input.substr(1, input.length() - 2));
+        }
+
+        // If the first and last characters don't match, it's not a palindrome
+        return false;
+    }
+};
+
+int main() {
+    // Create an instance of PalindromeChecker
+    PalindromeChecker checker;
+
+    // Test the isPalindrome method with various inputs
+    string testInputs[] = {"racecar", "madam", "hello", "level", "world", ""};
+
+    for (const string &input : testInputs) {
+        cout << "Is \"" << input << "\" a palindrome? " << (checker.isPalindrome(input) ? "Yes" : "No") << endl;
     }
 
-    if (input.charAt(0) == input.charAt(input.length() - 1)) {
-        return isPalindrome(input.substring(1, input.length() - 1));
-    }
-
-    return false;
+    return 0;
 }
