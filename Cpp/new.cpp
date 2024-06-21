9dadaa6a454bbf4fffee15fc1079964dbd141505
janakmallik <janaktoy@gmail.com>
2024-06-21 22:30:13 +0600

    new

---
+++ Cpp/new.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print() {
+  cout << "janak";
+}
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+int main() {
+  // print();
+  // int s = sum(1, 5);
+  // cout << s;
+  // return 0;
+
+  int basic[3] = {1, 3, 4};
+  array<int, 4> a = { 2, 4, 5, 6 };
+  int size  = a.size();
+
+  cout<<"2nd element-> "<<a.at(1)<<endl;
+  cout<<a.empty()<<endl;
+}
